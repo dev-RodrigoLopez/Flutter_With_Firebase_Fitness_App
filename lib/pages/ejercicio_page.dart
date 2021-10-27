49a55a5cfffb668290cc2a7ae3e34457051ee47f
Rodrigo Lopez <lopez_sarmiento@hotmail.com>
2021-10-27 09:44:50 -0500

    Se termino el Home page, se crea el ejercicioPage donde se agrego una barra indicadora y la imagen de fondo. Pantalla quedo a mitad del proceso

---
+++ lib/pages/ejercicio_page.dart
@@ -0,0 +1,103 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fittness_app/models/excercise_model.dart';
+import 'package:flutter/material.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+
+class EjercicioPage extends StatefulWidget {
+
+  final Exercise ejercicio;
+
+  const EjercicioPage({Key? key, required this.ejercicio}) : super(key: key);
+
+
+  @override
+  _EjercicioPageState createState() => _EjercicioPageState();
+}
+
+class _EjercicioPageState extends State<EjercicioPage> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Hero(
+          tag: widget.ejercicio.id,
+    
+          child: Stack(
+            children: [
+              CachedNetworkImage(
+                imageUrl: widget.ejercicio.thumbnail,
+                placeholder: ( context, url ) => Image(
+                  image: AssetImage( 'assets/no-image.jpg' ),
+                  fit: BoxFit.cover,
+                  height: size.height,
+                  width: size.width,
+                ) ,
+                errorWidget: ( context, url, error ) => Icon( Icons.error ),
+                fit: BoxFit.cover,
+                height: size.height,
+                width: size.width,
+              ),
+    
+              Container(
+                height: size.height,
+                width: size.width,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color( 0xFF000000 ),
+                      Color( 0x00000000),
+                    ],
+                  begin : Alignment.bottomCenter,
+                  end : Alignment.center
+                  ),
+                ),
+              ),
+    
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  width: size.width * 0.5,
+                  height: size.height * 0.4,
+                  child: SleekCircularSlider(
+                    appearance: CircularSliderAppearance(),
+                    onChange: (double value) {
+                      
+                    },
+                    initialValue: 30,
+                    min: 10,
+                    max: 60,
+                    innerWidget: (value){
+                      return Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '${value.toInt()} s',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 30
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              )
+    
+            ],
+          ) 
+    
+    
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:fittness_app/models/excercise_model.dart';
+import 'package:fittness_app/pages/ejercicio_page.dart';
 import 'package:fittness_app/providers/exercicios_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -23,6 +24,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
 
     final ejercicios = Provider.of<ExerciseProvider>(context).lEjercicio;
+    final size = MediaQuery.of( context ).size;
 
     return Scaffold(
       
@@ -37,24 +39,26 @@ class _HomePageState extends State<HomePage> {
             physics: BouncingScrollPhysics(),
             itemCount: ejercicios.length,
             itemBuilder: (context, index){
-              return Container( 
-                margin: EdgeInsets.only( top: 10, left: 10, right: 10 ) ,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
+              
+              return GestureDetector(
+                child: Hero(
+                  tag: ejercicios[index].id,
+                  child: _ItemList(ejercicio: ejercicios[index])
                 ),
+                onTap: (){
+                  print('Puto');
+                  Navigator.push( 
+                    context, 
+                    MaterialPageRoute(
+                      builder: ( context ) => EjercicioPage(
+                        ejercicio : ejercicios[index]
+                      )
+                    )
+                  );
 
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: FadeInImage(
-                    image: NetworkImage( ejercicios[index].thumbnail ),
-                    placeholder: AssetImage( 'assets/no-image.jpg' ),
-                    width: MediaQuery.of( context ).size.width,
-                    height: 250,
-                    fit: BoxFit.cover,
-                  ),
-                ),
+                },
+              );
 
-               );
             })
           : LinearProgressIndicator()
 
@@ -62,4 +66,73 @@ class _HomePageState extends State<HomePage> {
 
     );
   }
+}
+
+
+class _ItemList extends StatelessWidget {
+
+  final Exercise ejercicio;
+
+  const _ItemList({Key? key, required this.ejercicio}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final size = MediaQuery.of( context ).size;
+
+    return Container( 
+        margin: EdgeInsets.only( top: 10, left: 10, right: 10 ) ,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+        ),
+
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                image: NetworkImage( ejercicio.thumbnail ),
+                placeholder: AssetImage( 'assets/no-image.jpg' ),
+                width: size.width,
+                height: 250,
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Container(
+                width:size.width,
+                height: 250,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color( 0xFF000000 ),
+                      Color( 0x00000000 )
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.center
+                  )
+                ),
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.all(15),
+              height: 250,
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                ejercicio.title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            )
+
+          ],
+        ),
+
+        );
+    }
 }
